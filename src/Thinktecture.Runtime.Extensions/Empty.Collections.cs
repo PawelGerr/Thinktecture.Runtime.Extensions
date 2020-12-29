64417a40ed16008025cbcea750f94365a98dab55
Pawel Gerr <pawel.gerr@thinktecture.com>
2020-12-29 17:24:05 +0100

    Added "empty set"

--- src/Thinktecture.Runtime.Extensions/Empty.Collections.cs
+++ src/Thinktecture.Runtime.Extensions/Empty.Collections.cs
@@ -37,6 +37,17 @@ namespace Thinktecture
          return ReadOnlyDictionary<TKey, TValue>.Instance;
       }
 
+#if !NETSTANDARD2_1
+      /// <summary>
+      /// Returns empty set.
+      /// </summary>
+      /// <returns>An empty set.</returns>
+      public static IReadOnlySet<T> Set<T>()
+      {
+         return ReadOnlySet<T>.Instance;
+      }
+#endif
+
       /// <summary>
       /// Returns empty lookup.
       /// </summary>
@@ -102,5 +113,62 @@ namespace Thinktecture
             return GetEnumerator();
          }
       }
+
+#if !NETSTANDARD2_1
+      private class ReadOnlySet<T> : IReadOnlySet<T>
+      {
+         public static readonly IReadOnlySet<T> Instance = new ReadOnlySet<T>();
+
+         public int Count => 0;
+
+         public IEnumerator<T> GetEnumerator()
+         {
+            return Enumerable.Empty<T>().GetEnumerator();
+         }
+
+         IEnumerator IEnumerable.GetEnumerator()
+         {
+            return GetEnumerator();
+         }
+
+         public bool Contains(T item)
+         {
+            return false;
+         }
+
+         public bool IsProperSubsetOf(IEnumerable<T> other)
+         {
+            if (other is null)
+               throw new ArgumentNullException(nameof(other));
+
+            return other.Any();
+         }
+
+         public bool IsSubsetOf(IEnumerable<T> other)
+         {
+            return true;
+         }
+
+         public bool IsProperSupersetOf(IEnumerable<T> other)
+         {
+            return false;
+         }
+
+         public bool IsSupersetOf(IEnumerable<T> other)
+         {
+            return !other.Any();
+         }
+
+         public bool Overlaps(IEnumerable<T> other)
+         {
+            return false;
+         }
+
+         public bool SetEquals(IEnumerable<T> other)
+         {
+            return !other.Any();
+         }
+      }
+#endif
    }
 }
---
+++ test/Thinktecture.Runtime.Extensions.Tests/EmptyCollectionTests/Empty_Set.cs
@@ -0,0 +1,78 @@
+#if !NETCOREAPP3_1
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Thinktecture.EmptyCollectionTests
+{
+   // ReSharper disable once InconsistentNaming
+   public class Empty_Set
+   {
+      private IReadOnlySet<object> SUT => Empty.Set<object>();
+
+      private readonly HashSet<object> _realEmptySet = new();
+      private readonly HashSet<object> _realNonEmptySet = new() { 1 };
+
+      [Fact]
+      public void Should_not_be_null()
+      {
+         SUT.Should().NotBeNull();
+      }
+
+      [Fact]
+      public void Should_be_empty()
+      {
+         SUT.Should().BeEmpty();
+      }
+
+      [Fact]
+      public void Should_have_count_of_0()
+      {
+         SUT.Count.Should().Be(0);
+      }
+
+      [Fact]
+      public void Should_have_implementation_for_IsProperSubsetOf()
+      {
+         SUT.IsProperSubsetOf(_realEmptySet).Should().Be(_realEmptySet.IsProperSubsetOf(_realEmptySet));
+         SUT.IsProperSubsetOf(_realNonEmptySet).Should().Be(_realEmptySet.IsProperSubsetOf(_realNonEmptySet));
+      }
+
+      [Fact]
+      public void Should_have_implementation_for_IsSubsetOf()
+      {
+         SUT.IsSubsetOf(_realEmptySet).Should().Be(_realEmptySet.IsSubsetOf(_realEmptySet));
+         SUT.IsSubsetOf(_realNonEmptySet).Should().Be(_realEmptySet.IsSubsetOf(_realNonEmptySet));
+      }
+
+      [Fact]
+      public void Should_have_implementation_for_IsProperSupersetOf()
+      {
+         SUT.IsProperSupersetOf(_realEmptySet).Should().Be(_realEmptySet.IsProperSupersetOf(_realEmptySet));
+         SUT.IsProperSupersetOf(_realNonEmptySet).Should().Be(_realEmptySet.IsProperSupersetOf(_realNonEmptySet));
+      }
+
+      [Fact]
+      public void Should_have_implementation_for_IsSupersetOf()
+      {
+         SUT.IsSupersetOf(_realEmptySet).Should().Be(_realEmptySet.IsSupersetOf(_realEmptySet));
+         SUT.IsSupersetOf(_realNonEmptySet).Should().Be(_realEmptySet.IsSupersetOf(_realNonEmptySet));
+      }
+
+      [Fact]
+      public void Should_have_implementation_for_Overlaps()
+      {
+         SUT.Overlaps(_realEmptySet).Should().Be(_realEmptySet.Overlaps(_realEmptySet));
+         SUT.Overlaps(_realNonEmptySet).Should().Be(_realEmptySet.Overlaps(_realNonEmptySet));
+      }
+
+      [Fact]
+      public void Should_have_implementation_for_SetEquals()
+      {
+         SUT.SetEquals(_realEmptySet).Should().Be(_realEmptySet.SetEquals(_realEmptySet));
+         SUT.SetEquals(_realNonEmptySet).Should().Be(_realEmptySet.SetEquals(_realNonEmptySet));
+      }
+   }
+}
+
+#endif
