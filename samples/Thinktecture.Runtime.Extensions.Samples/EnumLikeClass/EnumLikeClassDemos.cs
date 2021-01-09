8c8628e69dc6f15bb18bfdcc44d2f4e406ba076a
Pawel Gerr <pawel.gerr@thinktecture.com>
2021-01-09 15:11:51 +0100

    Improved samples

--- samples/Thinktecture.Runtime.Extensions.Samples/EnumLikeClass/EnumLikeClassDemos.cs
+++ samples/Thinktecture.Runtime.Extensions.Samples/EnumLikeClass/EnumLikeClassDemos.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Serilog;
 
 namespace Thinktecture.EnumLikeClass
@@ -7,8 +8,46 @@ namespace Thinktecture.EnumLikeClass
    {
       public static void Demo(ILogger logger)
       {
+         DemoForNonValidatableEnum(logger);
+         DemoForValidatableEnum(logger);
+
+         logger.Information("Press ENTER to exit");
+         Console.ReadLine();
+      }
+
+      private static void DemoForNonValidatableEnum(ILogger logger)
+      {
+         logger.Information("==== Demo for IEnum<T> ====");
+
+         var productTypes = ProductType.Items;
+         logger.Information("Product types: {types}", productTypes);
+
+         var productType = ProductType.Get("Groceries");
+         logger.Information("Product type: {type}", productType);
+
+         if (ProductType.TryGet("Housewares", out var housewares))
+            logger.Information("Product type {type} with TryGet found", housewares);
+
+         string keyOfTheProductType = productType;
+         logger.Information("Implicit conversion of ProductType -> string: {key}", keyOfTheProductType);
+
+         try
+         {
+            ProductType.Get("Unknown");
+            logger.Warning("This line won't be reached.");
+         }
+         catch (KeyNotFoundException)
+         {
+            logger.Information("KeyNotFoundException is thrown because there is no product type with the key 'Unknown'.");
+         }
+      }
+
+      private static void DemoForValidatableEnum(ILogger logger)
+      {
+         logger.Information("==== Demo for IValidatableEnum<T> ====");
+
          var categories = ProductCategory.Items;
-         logger.Information("Catgories: {categories}", categories);
+         logger.Information("Categories: {categories}", categories);
 
          var category = ProductCategory.Get("Fruits");
          logger.Information("Category: {category}", category);
@@ -25,9 +64,6 @@ namespace Thinktecture.EnumLikeClass
          var unknownCategory = ProductCategory.Get("Grains");
          logger.Information("unknownCategory.Key: {categoryKey}", unknownCategory.Name);
          logger.Information("unknownCategory.isValid: {IsValid}", unknownCategory.IsValid);
-
-         logger.Information("Press ENTER to exit");
-         Console.ReadLine();
       }
    }
 }
