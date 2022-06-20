b3aef0efd498ad3881b57ebcfb6117b806d069f1
Pawel Gerr <pawel.gerr@thinktecture.com>
2022-06-20 12:40:27 +0200

    Convenience method for projecting read-only collections

--- src/Thinktecture.Runtime.Extensions/Extensions/ReadOnlyCollectionExtensions.cs
+++ src/Thinktecture.Runtime.Extensions/Extensions/ReadOnlyCollectionExtensions.cs
@@ -5,6 +5,22 @@ namespace Thinktecture;
 /// </summary>
 public static class ReadOnlyCollectionExtensions
 {
+   /// <summary>
+   /// Projects and wraps the provided <paramref name="items"/> and exposes the projected values as <see cref="IReadOnlyCollection{TResult}"/>.
+   /// </summary>
+   /// <param name="items">Items to project to <see cref="IReadOnlyCollection{TResult}"/>.</param>
+   /// <param name="selector">Returns a item of type <typeparamref name="TResult"/> for each item of type <typeparamref name="T"/>.</param>
+   /// <typeparam name="T">Item type.</typeparam>
+   /// <typeparam name="TResult">Result type.</typeparam>
+   /// <returns>A collection of type <see cref="IReadOnlyCollection{TResult}"/>.</returns>
+   /// <exception cref="ArgumentNullException">
+   ///   <paramref name="items"/> or <paramref name="selector"/> is <c>null</c>.
+   /// </exception>
+   public static IReadOnlyCollection<TResult> ToReadOnlyCollection<T, TResult>(this IReadOnlyCollection<T> items, Func<T, TResult> selector)
+   {
+      return items.Select(selector).ToReadOnlyCollection(items.Count);
+   }
+
 #if !NET6_0
    /// <summary>
    /// Splits the <paramref name="collection"/> in buckets of provided <paramref name="bucketSize"/>.
