b8ebe3c1330b1c7a0998d97bbc153ead5580eafe
Pawel Gerr <pawel.gerr@thinktecture.com>
2023-03-04 20:27:48 +0100

    Split the method "TrimOrNullify" in 2 for performance reasons.

--- src/Thinktecture.Runtime.Extensions/Extensions/StringExtensions.cs
+++ src/Thinktecture.Runtime.Extensions/Extensions/StringExtensions.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace Thinktecture;
 
 /// <summary>
@@ -5,6 +7,21 @@ namespace Thinktecture;
 /// </summary>
 public static class StringExtensions
 {
+   /// <summary>
+   /// If the <paramref name="text"/> is <c>null</c>, an empty <see cref="String"/> or contains whitespaces only, then <c>null</c> is returned.
+   /// Otherwise, the <paramref name="text"/> is trimmed.
+   /// </summary>
+   /// <param name="text">Text to trim or nullify.</param>
+   /// <returns>
+   /// Trimmed <paramref name="text"/>, if the <paramref name="text"/> is not <c>null</c> nor contains whitespaces only;
+   /// otherwise <c>null</c>.
+   /// </returns>
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   public static string? TrimOrNullify(this string? text)
+   {
+      return String.IsNullOrWhiteSpace(text) ? null : text.Trim();
+   }
+
    /// <summary>
    /// If the <paramref name="text"/> is <c>null</c>, an empty <see cref="String"/> or contains whitespaces only, then <c>null</c> is returned.
    /// Otherwise, the <paramref name="text"/> is trimmed and shortened according to <paramref name="maxLength"/>.
@@ -18,21 +35,18 @@ public static class StringExtensions
    /// <exception cref="ArgumentException">
    /// <paramref name="maxLength"/> is less or equals to 0.
    /// </exception>
-   public static string? TrimOrNullify(this string? text, int? maxLength = null)
+   public static string? TrimOrNullify(this string? text, int maxLength)
    {
+      if (maxLength <= 0)
+         throw new ArgumentException("The maximum length must be bigger than 0.", nameof(maxLength));
+
       if (String.IsNullOrWhiteSpace(text))
          return null;
 
       text = text.Trim();
 
-      if (maxLength.HasValue)
-      {
-         if (maxLength <= 0)
-            throw new ArgumentException("The maximum length must be bigger than 0.", nameof(maxLength));
-
-         if (text.Length > maxLength)
-            text = text[..maxLength.Value];
-      }
+      if (text.Length > maxLength)
+         text = text[..maxLength];
 
       return text;
    }
