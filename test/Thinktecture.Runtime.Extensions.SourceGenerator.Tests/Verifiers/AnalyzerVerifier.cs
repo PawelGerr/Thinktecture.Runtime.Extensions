dff9069769093592488996aa83eddbe8d0fc3620
Pawel Gerr <pawel.gerr@thinktecture.com>
2023-11-14 20:28:52 +0100

    Deps update

--- test/Thinktecture.Runtime.Extensions.SourceGenerator.Tests/Verifiers/AnalyzerVerifier.cs
+++ test/Thinktecture.Runtime.Extensions.SourceGenerator.Tests/Verifiers/AnalyzerVerifier.cs
@@ -47,7 +47,7 @@ public static class AnalyzerVerifier<TAnalyzer>
 #if NET7
          ReferenceAssemblies = new ReferenceAssemblies("net7.0", new PackageIdentity("Microsoft.NETCore.App.Ref","7.0.0"), Path.Combine("ref", "7.0.0"));
 #else
-         ReferenceAssemblies = new ReferenceAssemblies("net8.0", new PackageIdentity("Microsoft.NETCore.App.Ref", "8.0.0-rc.2.23479.6"), Path.Combine("ref", "8.0.0-rc.2.23479.6"));
+         ReferenceAssemblies = new ReferenceAssemblies("net8.0", new PackageIdentity("Microsoft.NETCore.App.Ref", "8.0.0"), Path.Combine("ref", "8.0.0"));
 #endif
          foreach (var additionalReference in additionalReferences)
          {
