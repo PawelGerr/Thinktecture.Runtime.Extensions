158af73d5888cecb30b8e41c2b94fd7e9f3d5cd3
Pawel Gerr <pawel.gerr@thinktecture.com>
2024-11-12 19:18:11 +0100

    Update deps

--- test/Thinktecture.Runtime.Extensions.Tests/ValueObjectTests/Create.cs
+++ test/Thinktecture.Runtime.Extensions.Tests/ValueObjectTests/Create.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using Thinktecture.Runtime.Tests.TestValueObjects;
 
 namespace Thinktecture.Runtime.Tests.ValueObjectTests;
@@ -71,8 +72,11 @@ public class Create
                                   {
                                      InitExpression = 1,
                                      InitBody = 2,
-                                     PublicPropertyDefaultInit = 3,
-                                     PrivatePropertyDefaultInit = 4
+                                     PublicPropertyDefaultInit = 3
                                   });
+
+      obj.GetType().GetProperty("PrivatePropertyDefaultInit", BindingFlags.Instance | BindingFlags.NonPublic)!
+         .GetValue(obj).Should().BeOfType<int>()
+         .Subject.Should().Be(4);
    }
 }
